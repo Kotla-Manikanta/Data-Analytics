e9ce2ce0e6ae7b2048fbaa0d83b9686fc4e1388a
kotla Manikanta <kotlamanikanta20@gmail.com>
2025-04-03 00:11:01 +0530

    added sample python code

---
+++ python_Basics.py
@@ -0,0 +1,41 @@
+# print('Hello world')
+
+# a=10
+# print(a)
+
+# b="Revature"
+# print(b)
+
+# name=input("enter your name: ")
+# print("My name is:",name)
+
+
+# name=input("enter your name: ")
+# age=int(input("enter your age:"))
+# address=input("Enter your address:")
+# print()
+# print("MY DETAILS--")
+# print("My name is:",name)
+# print("My age is:",age)
+# print("My address is:",address)
+
+# num1=int(input("enter num1:"))
+# num2=int(input("enter num2:"))
+# sum=num1+num2
+# print(f"sum={sum}")
+
+# print(type(num1))
+# a="kmk"
+# print(type(a))
+
+# num1=int(input("enter num1:"))
+# num2=int(input("enter num2:"))
+
+# print(f"sum={num1+num2}")
+# print(f"subtraction={num1-num2}")
+# print(f"multiplication={num1*num2}")
+# print(f"division={num1/num2}")
+# print(f"floor division={num1//num2}")
+# print(f"mod={num1%num2}")
+
+print("Hello", "world",sep="-")
