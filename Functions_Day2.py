6cc5c31e2aeaf5ca07c0fa918c398d11a6f659d0
kotla Manikanta <kotlamanikanta20@gmail.com>
2025-04-03 15:07:39 +0530

    added controlflow  file

---
+++ Functions_Day2.py
@@ -0,0 +1,6 @@
+#function
+def add(a,b):
+    return a+b
+result=add(5,2)
+print(result)
+
---
+++ controlstatements_Day2.py
@@ -0,0 +1,60 @@
+# if , else, elif
+
+# a=15
+# b=10
+# if a>b:
+#     print("a is greater")
+# elif a==b:
+#     print("both are equal")
+# else:
+#     print("b is greater")
+
+# i=0
+# if i!=0:
+#     if i>0:
+#         print("positive")
+#     if i<0:
+#         print("negative")
+# else:
+#     print("zero")
+
+# marks = int(input("Enter your marks: "))
+# if 90 <= marks <= 100:
+#     print("Grade A")
+# elif 70 <= marks <= 89:
+#     print("Grade B")
+# elif 60 <= marks <= 69:
+#     print("Grade C")
+# else:
+#     print("Fail")
+
+
+#looping statements
+# n=5
+# while n>0:
+#     if n==2:
+#         break
+#     print(n)
+#     n-=1
+    
+# n=5
+# while n>0:
+#     if n==2:
+#         continue
+#     print(n)
+#     n-=1
+
+# a=[10,40,55]
+# b=[1,5,9]
+# for i in a:
+#     for j in b:
+#         print(i,j)
+
+#Dictionary
+# a={'name':'kmk','age':25}
+# for i in a.keys():
+#     print(i)
+    
+# for i,j in a.items():
+#     print(i,j)
+
