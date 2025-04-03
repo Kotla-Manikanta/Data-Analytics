652a26c9d6f1fd88446eed500c8729b71cce141e
kotla Manikanta <kotlamanikanta20@gmail.com>
2025-04-03 11:09:53 +0530

    added datatypes file

---
+++ Datatypes_Day2.py
@@ -0,0 +1,61 @@
+# Strings
+# a="this is CIT college"
+# print(a[1])
+# print(a[3])
+# print(a[-1])
+# print(a[-5])
+# print(a[1:4])
+# print(a[::-1])
+
+# Lists
+# b=[1,2.5,"kmk"]
+# print(b)
+# print(b[0])
+# print(b[1])
+# print(b[2])
+# # print(b[4])  list index out of range 
+# b.append("new")
+# print(b)
+
+# Tuples
+# b=(1,2.5,"kmk")
+# print(b)
+# print(b[0])
+# print(b[1])
+# print(b[2])
+
+# Bool
+# valid=True
+# print(type(valid))  #<class 'bool'>
+
+# Set and frozenset
+# a={1,"kmk",3.6,'b'}
+# print(a)
+# s=set("Helloworld")
+# print(s)
+# s=set(["Hello","world","world"])
+# print(s)
+# b=frozenset({1,2,3})
+# print(b)
+# # b.add(5)
+# # print(b)
+
+# Range
+# b=range(1,10,2)
+# print(list(b))
+
+# Dictionary
+# a={'name':'kmk','age':25}
+# print(a['name'])
+# print(a['age'])
+# print(a.items())
+
+# Binary 
+# b=[65,66,67]
+# print(b)
+# c=bytes([65,66,67])  #ASCII values
+# print(c)
+
+# None
+# a=None
+# print(type(a))
