014d483f68a4fd0f1ffdee23c4b7c1c95beaf368
kotla Manikanta <kotlamanikanta20@gmail.com>
2025-04-04 10:55:08 +0530

    added decorator file

---
+++ Decorator.py
@@ -0,0 +1,12 @@
+
+def decorator(func):
+    def wrapper():
+        print('before calling')
+        func()
+        print('after calling')
+    return wrapper
+
+@decorator # greet=decorator(greet)
+def greet():
+    print('hello')
+greet()
